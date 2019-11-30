e84d0857f4796dfde2148452be7904344e2d0be1
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-11-30 19:47:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const person = ["Lisa", "Kaitlin", "Jan"];
+function writeCards(person,event){
+  const messArr = [];
+  for(let i = 0; i < friends.length; i++){
+    messages.push(`Thank you, ${friends[i]}, for the wonderful ${eName} gift!`);
+  }
+  return messages;
+}
+writeCards(friends, "birthday");
+
+function countdown(num){
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
+
+countdown(10);
